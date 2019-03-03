3eefebd6a8f944aa4fcc1cf60154b9a1bcdb27b4
pdevine2 <47839141+pdevine2@users.noreply.github.com>
2019-03-02 21:38:10 -0500

    Add files via upload
    Added code to count totals from pyYear added from lyrics.wikia.com. Added code to combine totals from Year and pyYear, then write to CSV file YearTotals.

--- CalcCSVTotals.py
+++ CalcCSVTotals.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 13 21:47:32 2019
-Generate the following statistics using the metadata associated with each
-lyric record for all cleaned CSV files A(2).csv to Z(2).csv and Numbers(20).csv.
+CalcCSVTotals: Generate the following statistics using the metadata associated 
+               with each lyric record for all cleaned CSV files A(2).csv to 
+               Z(2).csv and Numbers(20).csv.
 
 Note: Prototype version to be used in the Spyder IDE to access and display the
 the following dictionaries. Have not yet generated useful console or file output.
@@ -71,32 +72,38 @@ def main():
             for inputCSV_file in inputCSV_list:
                 inputCSV_file = inputCSV_file.strip()
                 
-                with open(inputCSV_file, 'r') as inputCSV:
-                    inputCSVrows = inputCSV.readlines()
+                with open(inputCSV_file, 'rb') as inputCSV:
+                    try:
+                        inputCSVrows = inputCSV.readlines()
                     
-                    for row in inputCSVrows:
-                        if killer.kill_now == True:
-                            print("exit_state TRUE")
-                            break
-                        try:
-                            rowList = row.split(',')
-                            updateDict(genreDict, rowList, 1)
-                            updateDict(artistDict, rowList, 0)
-                            updateDict(SongYearDict, rowList, 4)
-                            
-                            updateDict2(genreYearDict,rowList,1,4)
-                            updateDict2(artistYearDict,rowList,0,4)
-                            
-                            updateDictbyRating(artistbyRatingGenre,rowList,5)
-                            updateDictbyRating(artistbyRating2Genre,rowList,2)
-
-                            
-                        except:
-                            status += 1
-                            continue
+                        for row in inputCSVrows:
+                            if killer.kill_now == True:
+                                print("exit_state TRUE")
+                                break
+                            try:
+                                rowList = row.decode('ascii').rstrip().split(',')
+                                #rowList = row.split(',')
+                                #updateDict(genreDict, rowList, 1)
+                                #updateDict(artistDict, rowList, 0)
+                                updateDict(SongYearDict, rowList, 4)
+                                updateDict(SongpyYearDict, rowList, 7)
+                             
+                                #updateDict2(genreYearDict,rowList,1,4)
+                                #updateDict2(artistYearDict,rowList,0,4)
+                             
+                                #updateDictbyRating(artistbyRatingGenre,rowList,5)
+                                #updateDictbyRating(artistbyRating2Genre,rowList,2)
+ 
+                            except:
+                                status += 1
+                                continue
+                    except:
+                        status += 1
+                        continue
+                    
     else:
         print("\nUsage: python CalcCSVTotals.py InpuCSVs.txt\n")
-
+    return status
 
 if __name__ == '__main__':
     killer = GracefulKiller()
@@ -105,6 +112,7 @@ if __name__ == '__main__':
         
         genreDict = {}
         SongYearDict = {}
+        SongpyYearDict = {}
         artistDict = {}
         genreYearDict = {}
         artistYearDict = {}
@@ -112,7 +120,23 @@ if __name__ == '__main__':
         artistbyRating2Genre = {}
         artistbyRatingYear = {}
         status = main()
-        print(genreDict)
+        new_dict = {}
+        for key, value in SongYearDict.items():
+            new_dict[key] = value
+        for key, value in SongpyYearDict.items():
+            if key in new_dict:
+                new_dict[key] = [new_dict[key], value]
+            else:
+                new_dict[key] = [0, value]
+        status = 0
+        with open('YearTotals.csv', 'w', newline='') as outfile:
+            for key,value in new_dict.items():
+                try:
+                    outfile.write(str(key) + ',' + str(value[0]) + ',' + str(value[1]) + '\n')
+                except:
+                    status += 1
+                    continue
+                
     except KeyboardInterrupt:
         pass
     
